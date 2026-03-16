70b49fbfa0c996def69dcc400923468933f59821
Windelle JC <windellejcr@gmail.com>
2026-03-16 21:27:55 +0800

    Configure static export for Cloudflare Pages

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,6 +2,8 @@ import type { MetadataRoute } from "next";
 
 import { siteUrl } from "@/data/site-content";
 
+export const dynamic = "force-static";
+
 export default function robots(): MetadataRoute.Robots {
   const resolvedSiteUrl = new URL(siteUrl);
 
